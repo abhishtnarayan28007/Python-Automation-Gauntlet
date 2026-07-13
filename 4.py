6df9dffedce8a27f92762cdfd7018719a612c1b5
Abhisht Narayan <abhishtnarayan007@gmail.com>
2026-07-13 12:58:07 +0530

    Complete Problem 4 flattening logic and optimized properties export

--- 4.py
+++ 4.py
@@ -1,10 +1,22 @@
 import json
+flattened_data = []
 with open("app_config.json","r") as file:
     data = json.load(file)
-flat_strings = []
 for main_item , main_val in data.items():
     if type(main_val) == dict:
         for inner_item , inner_val in main_val.items():
-            print(f"")
+             a= main_item +'.'+ inner_item +'='+ str(inner_val)
+             flattened_data.append(a)
+    else:
+        b = main_item +'='+ str(main_val)
+        flattened_data.append(b)
+with open("config.properties","a",newline="") as file2:
+    for item in flattened_data:
+        file2.write(item + "\n")
+print("success")
+
+
+
+            
 
 
